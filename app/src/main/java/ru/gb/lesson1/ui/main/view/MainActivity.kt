e13bb107eeced92cba42493bd34f2a5dd0910ec9
Egor M <merkushev.eg@gmail.com>
2021-11-03 20:53:55 +0200

    bottom nav view has been implemented

--- app/src/main/java/ru/gb/lesson1/ui/main/view/MainActivity.kt
+++ app/src/main/java/ru/gb/lesson1/ui/main/view/MainActivity.kt
@@ -3,21 +3,59 @@ package ru.gb.lesson1.ui.main.view
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatDelegate
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.main_activity.*
 import ru.gb.lesson1.R
+import ru.gb.lesson1.databinding.MainActivityBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private val binding: MainActivityBinding by lazy {
+        MainActivityBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
-
         setCurrentTheme()
 
+        setContentView(binding.root)
+
+        initBottomNavigationView()
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, MainFragment.newInstance())
                 .commitNow()
+        }
+    }
+
+    private fun initBottomNavigationView() {
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        bottomNavigationView.selectedItemId = R.id.home
+        binding.bottomNavigation.setOnItemSelectedListener { item ->
+            when(item.itemId) {
+                R.id.home -> {
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.container, MainFragment.newInstance())
+                        .commit()
+                    true
+                }
+                R.id.explore ->{
+                    Toast.makeText(this, "Explore", Toast.LENGTH_SHORT).show()
+                    true
+                }
+
+                R.id.settings ->{
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.container, SettingsFragment.newInstance())
+                        .commit()
+                    true
+                }
+                else -> false
+            }
         }
     }
 
