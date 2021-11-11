c3303812c1580d51af6878a844833675654e285c
Egor M <merkushev.eg@gmail.com>
2021-11-11 13:57:28 +0200

    Lesson 5 init

--- app/src/main/java/ru/gb/lesson1/ui/main/view/mars/MarsFragment.kt
+++ app/src/main/java/ru/gb/lesson1/ui/main/view/mars/MarsFragment.kt
@@ -7,7 +7,6 @@ import android.view.View
 import android.view.ViewGroup
 import kotlinx.android.synthetic.main.mars_fragment.*
 import ru.gb.lesson1.R
-
 import ru.gb.lesson1.databinding.MarsFragmentBinding
 
 
