c3d52aea26f032ed534ce4c6ed1da9034ab31e03
Egor M <merkushev.eg@gmail.com>
2021-12-07 15:55:56 +0200

    lesson8

--- app/src/main/java/ru/gb/lesson1/ui/main/view/MainActivity.kt
+++ app/src/main/java/ru/gb/lesson1/ui/main/view/MainActivity.kt
@@ -18,6 +18,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.MainTheme)
         super.onCreate(savedInstanceState)
         setCurrentTheme()
 
